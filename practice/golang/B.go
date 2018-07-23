d22b17533e7b6783e0ef15ee75062afa946b3ea7
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2018-07-23 17:52:41 +0900

    作成中

--- practice/golang/B.go
+++ practice/golang/B.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+var writer = bufio.NewWriter(os.Stdout)
+
 func main() {
-	fmt.Print("under constructing...")
+	var N, Q int
+	var ans string
+
+	fmt.Scan(&N, &Q)
+	fmt.Fprintln("%d %d", N, Q)
+	
+	fmt.Fprintln(writer, "? A B")
+	_ = writer.Flush()
+
+	fmt.Scan(&ans)
+	fmt.Printf("test %s", ans)
 }
