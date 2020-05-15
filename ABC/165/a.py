8711e52b1229b7b3d00b7c889d3be1f741dc8586
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2020-05-15 13:03:58 +0900

    ABC165_A

---
+++ ABC/165/a.py
@@ -0,0 +1,13 @@
+K = int(input())
+
+A, B = map(int, input().split())
+
+if A // K == B // K:
+    if A % K == 0 or B % K == 0:
+        print("OK")
+    else:
+        print("NG")
+elif A // K < B // K:
+    print("OK")
+else:
+    print("NG")
